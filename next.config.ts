ad372d7f9babd5a076951bd838823b58ea87d130
MS-Kwak <kkminseo@gmail.com>
2025-06-23 09:05:08 +0900

    emotion diary supabase  초기 세팅 커밋!

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true, // Log full URLs for fetch requests
+    },
+  },
+  images: {
+    domains: ['[project_id].supabase.co'],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '1gb', // 여기서 크기를 조정하세요
+    },
+  },
 };
 
 export default nextConfig;
